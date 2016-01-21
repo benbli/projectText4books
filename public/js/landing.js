c215c6dae513d8e17f40b0668db3187102125fa5
Terrell Quillin <terrellq@gmail.com>
2016-01-21 11:44:09 -0500

    Styles and centers landing page.

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,35 +1,35 @@
-console.log("landing");
-
-$(function(){
-  $('.sign-up').hide();
-  $('#login').hide();
-  renderLogForm();
-  // renderSignForm();
-  // renderSignFormTwo();
-});
-
-// this works
-function renderLogForm(){
-  $('#already-user').on('click', function(e){
-    e.preventDefault();
-    $('#login').show();
-  });
-}
-// function renderSignForm(){
-//   console.log('creer an account');
-//   $('#create-account').on('click', function(e){
-//     e.preventDefault();
-//     console.log('cela marche create account? ');
-//     $('.sign-up').show();
-//     console.log('cela marche create account? part deux');
+// console.log("landing");
 //
-//       function renderSignFormTwo(){
-//         console.log("est-ce que cela marche?");
-//           $('#signup-submit').on('click', function(e){
-//               e.preventDefault();
-//               console.log("oui ou non");
-//               $('#login').show();
-//           });
-//       }
-//     });
+// $(function(){
+//   // $('.sign-up').hide();
+//   // $('#login').hide();
+//   renderLogForm();
+//   // renderSignForm();
+//   // renderSignFormTwo();
+// });
+//
+// // this works
+// function renderLogForm(){
+//   $('#already-user').on('click', function(e){
+//     e.preventDefault();
+//     $('#login').show();
+//   });
 // }
+// // function renderSignForm(){
+// //   console.log('creer an account');
+// //   $('#create-account').on('click', function(e){
+// //     e.preventDefault();
+// //     console.log('cela marche create account? ');
+// //     $('.sign-up').show();
+// //     console.log('cela marche create account? part deux');
+// //
+// //       function renderSignFormTwo(){
+// //         console.log("est-ce que cela marche?");
+// //           $('#signup-submit').on('click', function(e){
+// //               e.preventDefault();
+// //               console.log("oui ou non");
+// //               $('#login').show();
+// //           });
+// //       }
+// //     });
+// // }
